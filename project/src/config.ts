2367192ee69a9072e4698f62783d05b8dfc5e03f
aliaksei_dzemidovich <ladyblaumeux24@gmail.com>
2023-02-28 18:15:27 +0500

    fix: bug

--- project/src/config.ts
+++ project/src/config.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "https://react-learnwords-example.herokuapp.com";
-export const CUSTOM_API_BASE_URL = "https://rs-lang-team-154.herokuapp.com";
+export const API_BASE_URL = "https://rsback.onrender.com";
+export const CUSTOM_API_BASE_URL = "https://rsback.onrender.com/";
 export const AUTH_KEY = "auth";
 
 export const SELECTION_DATA = [
